7b9d69bf155cdd585f3572e1078e33e95a19fad5
Ruowen Liu <ruowen.liu@tuni.fi>
2025-12-20 11:52:29 +0200

    Themes defined

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return <div className="min-h-screen">
+        {/* Theme toggle: light or dark mode, but last thing to do */}
+        
+        {/* Background Effects */}
+
+        {/* NavBar */}
+
+        {/* Main Section */}
+
+
+        {/* Footer */}
+    </div>;
+};
